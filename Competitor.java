e69c1b0bbcc514cbc075358102029ed5216c8479
DinosZ <88870835+DinosZ@users.noreply.github.com>
2026-02-11 20:37:11 +0700

    Add files via upload

---
+++ Competitor.java
@@ -0,0 +1,132 @@
+// File: Competitor
+// Description: to show all competitor score
+// Assignment Number: lab05
+//
+// ID: 6888040
+// Name: Worapol Khaeso
+// Section: 2
+// Grader: <YOUR GRADER’S NAME>
+//
+// On my honor, Worapol Khaeso, this lab assignment is my own work
+// and I have not provided this code to any other students.
+
+import java.util.*;
+
+public class Competitor {
+	
+	private String fname, lname, nationality;
+	private int age;
+	private List<Score> score;
+	private double w1, w2;
+	private final int maxKpoint = 50;
+	
+	public Competitor(double w1, double w2) {
+		fname = lname = nationality = "";
+		age = 0;
+		score = new ArrayList<>();
+		this.w1 = w1;  this.w2 = w2;
+	}
+	
+	//Set all attributes to the given parameters.
+	public void setProfile(String fname, String lname, String nationality, int age)
+	{
+		//TODO: Code HERE
+		this.fname = fname;
+		this.lname = lname;
+		this.nationality = nationality;
+		this.age = age;
+	}
+	
+	// Add jScore and sScore to the list of score
+	// The first score will represent the round1 score for the competitor
+	public void setScore(double jScore, double[] sScore) {
+		//TODO: Code HERE
+		// สร้าง Object Score ใหม่เพื่อเก็บคะแนนในแต่ละรอบ
+		Score newScore = new Score();
+		newScore.setScores(jScore);
+	    newScore.setScores(sScore);
+	    this.score.add(newScore); // เพิ่มลงใน List เพื่อรองรับการแข่งขันหลายรอบ
+	}
+	
+	public void showProfile() {
+		System.out.print("Name: "+this.fname+" "+this.lname+", Age:"+this.age+", Nationality:"+this.nationality);
+	}
+	
+	// Display all raw score both (1) jump length and (2) Aerial Style from the given input round
+	// [be careful] The input round must start from 1 not 0.
+	// The format is present in Lab06 sheet
+	public void showRawScore(int round) {
+		//TODO: Code HERE
+		Score s = this.score.get(round - 1);
+	    System.out.print("Jumping score: " + getJScore(round));
+	    System.out.print("\nAriel Style (Raw) score: ");
+	    for (double val : s.getStyleScore()) {
+	        System.out.print(val + "|");
+	    }
+	}
+	
+	
+   /*
+    * Return the Jump Score (JScore) for the given round
+	*  JScore = (jump length/maxKpoint)*10
+    */
+	public double getJScore(int round) {
+		//TODO: Code HERE
+		return (this.score.get(round - 1).getJumpScores() / maxKpoint) * 10;
+	}
+	
+   /*
+    * Return the final score (FScore) for the given round
+    * jScpre comes from getJSCore (Score.java)
+    * sScore comes from getCalibratedAverage (Score.java)
+    * FScore = jScore*w1 + sScore*w2
+    */
+	public double getFinalScore(int round) {
+		//TODO: Code HERE
+		double jScore = getJScore(round); // คะแนนระยะกระโดด
+		double sScore = this.score.get(round - 1).getCalibratedAverage(); // คะแนนสไตล์
+		return (jScore * w1) + (sScore * w2);
+	}
+	
+	//This main is for testing your output
+	public static void main(String[] args) {
+		Competitor c1 = new Competitor(0.5, 0.5);
+		Competitor c2 = new Competitor(0.2, 0.8);
+		
+		c1.setProfile("Somechai", "Jaidee", "Thai", 33);
+		c2.setProfile("Brain", "Toss", "British", 35);
+		
+		System.out.println("===============================");
+		//Competitor 1
+		c1.setScore(45, new double[]{8.0,8.5,9.0,7.5,6.7,8.0});  //Round1
+		c1.setScore(49, new double[]{10.0,8.5,9.0,7.5,6.7,8.0}); //Round2
+		
+		c1.showProfile();
+		System.out.println("\n[Round1]");
+		c1.showRawScore(1);
+		System.out.format("\nFinal score: %.2f",c1.getFinalScore(1));
+		
+		System.out.println("\n[Round2]");
+		c1.showRawScore(2);
+		System.out.format("\nFinal score: %.2f",c1.getFinalScore(2));
+		
+		System.out.println("\n===============================");
+		
+		//Competitor 2
+		c2.setScore(37, new double[]{9.0,9.5,9.0,7.5,8.7,8.0});  //Round1
+		c2.setScore(48, new double[]{9.3,8.5,9.5,8.5,7.7,8.0}); //Round2
+		
+		c2.showProfile();
+		System.out.println("\n[Round1]");
+		c2.showRawScore(1);
+		System.out.format("\nFinal score: %.2f",c2.getFinalScore(1));
+		
+		System.out.println("\n[Round2]");
+		c2.showRawScore(2);
+		System.out.format("\nFinal score: %.2f",c2.getFinalScore(2));
+		
+			
+	}
+	
+	
+}
---
+++ Score.java
@@ -0,0 +1,76 @@
+/*
+* This class stores scores and provide calibrated average about the scores from 6 judges. 
+* This class will be used as a part of the Competitor class. 
+*/
+import java.util.Arrays;
+import java.util.List;
+
+public class Score {
+	private double jumpScore;
+	private double[] styleScore; 
+	
+	//The constructor should set all given attributes to 0.
+	public Score() {
+		//TODO: Code HERE
+		this.jumpScore = 0.0;
+		this.styleScore = new double[6];
+	}
+	
+	//This method set the jumpScore to the given score
+	public void setScores(double score) {
+		//TODO: Code HERE
+		this.jumpScore = score;
+	}
+	
+	//This method set the styleScore to the given score
+	public void setScores(double[] scores) {
+		//TODO: Code HERE
+		this.styleScore = scores;
+	}
+	
+	//This method return the jumpScore
+	public double getJumpScores() {
+		//TODO: Code HERE
+		return this.jumpScore;
+	}
+	
+	//This method return the styleScore
+	public double[] getStyleScore() {
+		//TODO: Code HERE
+		return this.styleScore;
+	}
+	
+   /*
+    * Return the average of the scores after excluding the minimum and maximum
+    * value. In short, it should return 
+    *   (SUM(this.scores) - MIN(this.scores) - MAX(this.scores))/(n-2), where
+    * n is the length of the score list. 
+    */
+	public double getCalibratedAverage() {
+		
+		//TODO: Code HERE
+		double min = styleScore[0];
+        double max = styleScore[0];
+        double sum = 0;
+        
+        for (double s : styleScore) {
+            // วนลูปหาค่าที่น้อยที่สุดด้วยตัวเอง 
+            if (s < min) min = s;
+            // วนลูปหาค่าที่มากที่สุดด้วยตัวเอง 
+            if (s > max) max = s;
+            // รวมคะแนนทั้งหมด 
+            sum += s;
+        }
+		return (sum - min - max) / (styleScore.length - 2); 
+	}
+	
+	//This main is for testing your output
+	public static void main(String[] args) {
+		Score s1 = new Score();
+		s1.setScores(9.5);
+		s1.setScores(new double[] {5.5, 4.5, 3.5, 2.5, 1.5, 6.5});
+		System.out.println(s1.getJumpScores());
+		System.out.println(Arrays.toString(s1.getStyleScore()));
+		System.out.println(s1.getCalibratedAverage());
+	}
+}
